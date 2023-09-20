d30ed970880346641cf7a985da98467cddb5a671
Lindsey Bartges <lrbartges@gmail.com>
2023-09-20 14:37:11 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// Function to write thank you cards
+function writeCards(names, event) {
+  const thankYouMessages = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return thankYouMessages;
+}
+
+// Function to count down from a given number
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+module.exports = {
+  writeCards,
+  countDown,
+};
+
